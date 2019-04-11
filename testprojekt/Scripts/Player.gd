38435a4c43ddc99c77c874b91088d4cf7e169b01
klaegkev <klaegkev@students.zhaw.ch>
2019-04-11 19:35:40 +0200

    tests

--- testprojekt/Scripts/Player.gd
+++ testprojekt/Scripts/Player.gd
@@ -1,71 +1,80 @@
 extends KinematicBody2D
 
-const GRAVITY = 981
-const WALK_SPEED = 400
-const JUMP_SPEED = -500
+const GRAVITY = 1500
+const SPEED = 500
+const JUMP_SPEED = -700
 
-var velocity = Vector2()
+var motion = Vector2()
+
 enum DIRECTION { RIGHT = 1, LEFT = -1}
 var direction = DIRECTION.RIGHT
 
-var jump = false
-var jumpCancel = false
+var jumpTime = 0
+var canJump = true
 
-var timer
 var canDash = true
+var dashCooldown = 3.0
+var dashCooldownTimer
+
+var dashParticleSpeed = -500
 
 func _ready():
-	timer = Timer.new()
-	timer.one_shot = true
-	timer.wait_time = 3.0
-	self.add_child(timer)
-	timer.connect("timeout", self, "timer_onTimeout")
-	
-func timer_onTimeout():
-	$AnimatedSprite.stop()
+	dashCooldownTimer = Timer.new()
+	dashCooldownTimer.one_shot = true
+	dashCooldownTimer.wait_time = dashCooldown
+	self.add_child(dashCooldownTimer)
+	dashCooldownTimer.connect("timeout", self, "on_dashCooldownTimer_timeout")
+
+func on_dashCooldownTimer_timeout():
 	canDash = true
 
-func _process(delta):
-	if(is_on_floor()):
-		velocity.y = 0;
-	else:
-		velocity.y += delta * GRAVITY
-	
-	if Input.is_key_pressed(KEY_A):
-		velocity.x = -WALK_SPEED
+func _physics_process(delta):
+	# GRAVITY
+	motion.y += delta * GRAVITY
+		
+	# LEFT / RIGHT MOVEMENT
+	if Input.is_key_pressed(KEY_D):
+		if direction == DIRECTION.LEFT:
+			self.scale.x *= -1
+			direction = DIRECTION.RIGHT
+
+		motion.x = SPEED
+	elif Input.is_key_pressed(KEY_A):
 		if direction == DIRECTION.RIGHT:
 			self.scale.x *= -1
 			direction = DIRECTION.LEFT
 
-	if Input.is_key_pressed(KEY_D):
-		velocity.x = WALK_SPEED
-		if direction == DIRECTION.LEFT:
-			self.scale.x *= -1
-			direction = DIRECTION.RIGHT
-	if (Input.is_key_pressed(KEY_SPACE)):
-		velocity.y = JUMP_SPEED
-		jump = true
-	if (is_on_ceiling()):
-		velocity.y = delta * GRAVITY
+		motion.x = -SPEED
+	else:
+		motion.x = 0
 	
+	# DASH
 	if Input.is_key_pressed(KEY_E) && canDash:
 		canDash = false
-		$AnimatedSprite.play("default")
-		timer.start()
-		self.translate(Vector2(500.0 * direction, 0.0))
-		velocity.y = delta * GRAVITY
-		
-	move_and_slide(velocity, Vector2(0, -1))
-	velocity.x = 0
+		dashCooldownTimer.start()
+		$Particles2D.set_as_toplevel(true)
+		$Particles2D.position = self.position
+		var material : ParticlesMaterial = $Particles2D.process_material
+		material.initial_velocity = dashParticleSpeed * direction
+		$Particles2D.emitting = true
+		self.position.x += direction * 400
+
+	# JUMPING
+	if Input.is_key_pressed(KEY_SPACE) && jumpTime < 0.3 && canJump:
+		motion.y = JUMP_SPEED
+		jumpTime += delta
+	elif !Input.is_key_pressed(KEY_SPACE) && jumpTime > 0:
+		canJump = false
 	
+	if is_on_floor():
+		jumpTime = 0
+		canJump = true
+	
+	motion = move_and_slide(motion, Vector2(0, -1))
+
 func attack():
 	# todo
 	pass
 	
 func takeDamage(damage):
 	$StateManager.take_Damage(damage)
-	pass
-
-func _on_AnimatedSprite_animation_finished():
-	$AnimatedSprite.stop()
-	$AnimatedSprite.frame = 0
