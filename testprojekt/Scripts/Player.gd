42961e91ede45713f331d6cacb0dec5c2d2908e1
klaegkev <klaegkev@students.zhaw.ch>
2019-04-11 09:12:02 +0200

    test

--- testprojekt/Scripts/Player.gd
+++ testprojekt/Scripts/Player.gd
@@ -1,12 +1,29 @@
 extends KinematicBody2D
 
-const GRAVITY = 1200
+const GRAVITY = 981
 const WALK_SPEED = 400
-const JUMP_SPEED = -900
+const JUMP_SPEED = -500
 
 var velocity = Vector2()
-enum DIRECTION { RIGHT, LEFT}
+enum DIRECTION { RIGHT = 1, LEFT = -1}
 var direction = DIRECTION.RIGHT
+
+var jump = false
+var jumpCancel = false
+
+var timer
+var canDash = true
+
+func _ready():
+	timer = Timer.new()
+	timer.one_shot = true
+	timer.wait_time = 3.0
+	self.add_child(timer)
+	timer.connect("timeout", self, "timer_onTimeout")
+	
+func timer_onTimeout():
+	$AnimatedSprite.stop()
+	canDash = true
 
 func _process(delta):
 	if(is_on_floor()):
@@ -25,9 +42,17 @@ func _process(delta):
 		if direction == DIRECTION.LEFT:
 			self.scale.x *= -1
 			direction = DIRECTION.RIGHT
-	if (Input.is_key_pressed(KEY_SPACE) && is_on_floor()):
+	if (Input.is_key_pressed(KEY_SPACE)):
 		velocity.y = JUMP_SPEED
+		jump = true
 	if (is_on_ceiling()):
+		velocity.y = delta * GRAVITY
+	
+	if Input.is_key_pressed(KEY_E) && canDash:
+		canDash = false
+		$AnimatedSprite.play("default")
+		timer.start()
+		self.translate(Vector2(500.0 * direction, 0.0))
 		velocity.y = delta * GRAVITY
 		
 	move_and_slide(velocity, Vector2(0, -1))
@@ -40,28 +65,7 @@ func attack():
 func takeDamage(damage):
 	$StateManager.take_Damage(damage)
 	pass
-	
 
-# das wär en prototyp , für en dash
-#var dash_timer
-#var can_dash = true
-#
-#func _ready():
-#	dash_timer = Timer.new()
-#	dash_timer.wait_time = 5.0
-#	dash_timer.one_shot = true
-#	self.add_child(dash_timer)
-#	dash_timer.connect("timeout", self, "cooldown_reset")
-
-#func cooldown_reset():
-#	can_dash = true
-
-
-#if can_dash && Input.is_key_pressed(KEY_E):
-#		self.translate(Vector2(550, 0))
-#		can_dash = false
-#		dash_timer.start()
-#	if can_dash && Input.is_key_pressed(KEY_Q):
-#		self.translate(Vector2(-550, 0))
-#		can_dash = false
-#		dash_timer.start()
+func _on_AnimatedSprite_animation_finished():
+	$AnimatedSprite.stop()
+	$AnimatedSprite.frame = 0
