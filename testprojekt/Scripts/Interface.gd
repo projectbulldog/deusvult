6548527e508b52105eb50ec41d2697803e6ab160
Kevin Kläger <kevin.klaeger@thurweb.ch>
2019-05-09 13:36:43 +0200

    test texture bar

--- testprojekt/Scripts/Interface.gd
+++ testprojekt/Scripts/Interface.gd
@@ -3,3 +3,5 @@ extends CanvasLayer
 func _on_StateManager_on_health_changed(health):
 	$Tween.interpolate_property($ProgressBar, "value", $ProgressBar.value, health, 0.2, Tween.TRANS_SINE, Tween.EASE_OUT)
 	$Tween.start()
+	
+	$TextureProgress.value = health
